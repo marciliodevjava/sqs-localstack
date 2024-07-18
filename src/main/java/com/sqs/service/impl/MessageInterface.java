d78122a2f1e9ca8197e134f1e2cae89175bf8aba
marciliodevjava <marciliodevjava@gmail.com.br>
2024-07-18 20:37:13 -0300

    Atualização do MessageInterface

--- src/main/java/com/sqs/service/impl/MessageInterface.java
+++ src/main/java/com/sqs/service/impl/MessageInterface.java
@@ -3,5 +3,7 @@ package com.sqs.service.impl;
 import com.sqs.record.MyMessage;
 
 public interface MessageInterface {
-    Boolean send(MyMessage messageSendDto);
+    Boolean sendSqs(MyMessage messageSendDto);
+
+    Boolean sendSns(MyMessage message);
 }
