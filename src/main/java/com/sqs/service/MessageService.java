5ffd090f7e4f88f2793e6447794888aa86f6f8ca
marciliodevjava <marciliodevjava@gmail.com.br>
2024-07-18 20:36:56 -0300

    Atualização do MessageService

--- src/main/java/com/sqs/service/MessageService.java
+++ src/main/java/com/sqs/service/MessageService.java
@@ -7,6 +7,9 @@ import io.awspring.cloud.sqs.operations.SqsTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import software.amazon.awssdk.services.sns.SnsClient;
+import software.amazon.awssdk.services.sns.model.PublishRequest;
+import software.amazon.awssdk.services.sns.model.PublishResponse;
 
 import java.util.Objects;
 
@@ -14,13 +17,33 @@ import java.util.Objects;
 public class MessageService implements MessageInterface {
     @Autowired
     private SqsTemplate sqsTemplate;
-    @Value("${SQS.0001}")
+    @Autowired
+    private SnsClient snsClient;
+
+    @Value("${sqs.name.sqs-0001}")
     private String SQS_1;
+    @Value("${sns.name.sns-0001}")
+    private String SNS_1;
 
     @Override
-    public Boolean send(MyMessage messageSendDto) {
+    public Boolean sendSqs(MyMessage messageSendDto) {
         SendResult messageSend = sqsTemplate.send(SQS_1, messageSendDto);
-        if(Objects.nonNull(messageSend.messageId())) return true;
+        if (Objects.nonNull(messageSend.messageId())) return true;
         return false;
+    }
+
+    @Override
+    public Boolean sendSns(MyMessage message) {
+        try {
+            PublishRequest publishRequest = PublishRequest.builder()
+                    .topicArn(SNS_1)
+                    .message(String.valueOf(message)) // Certifique-se de que MyMessage tenha um método getMessage()
+                    .build();
+
+            PublishResponse publishResponse = snsClient.publish(publishRequest);
+            return Objects.nonNull(publishResponse.messageId());
+        } catch (Exception e) {
+            return false;
+        }
     }
 }
