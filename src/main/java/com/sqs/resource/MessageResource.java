8b0fd36bf0c17f147d59089a62b26fa10e31421d
marciliodevjava <marciliodevjava@gmail.com.br>
2024-07-18 20:37:05 -0300

    Atualização do MessageResource

--- src/main/java/com/sqs/resource/MessageResource.java
+++ src/main/java/com/sqs/resource/MessageResource.java
@@ -9,6 +9,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 @RequestMapping("/message")
 public class MessageResource {
@@ -16,10 +19,27 @@ public class MessageResource {
     @Autowired
     private MessageInterface messageInterface;
 
-    @PostMapping
-    public ResponseEntity send(@RequestBody MyMessage message) {
-        Boolean messageSend = messageInterface.send(message);
-        if (messageSend) return ResponseEntity.status(201).body("Mensagem enviada com sucesso.");
-        return ResponseEntity.status(503).body("Mensagem não enviada.");
+    @PostMapping("/sqs")
+    public ResponseEntity sendSqs(@RequestBody MyMessage message) {
+        Boolean messageSend = messageInterface.sendSqs(message);
+        Map<String, String> response = new HashMap<>();
+        if (messageSend){
+            response.put("message", "Mensagem SQS enviada com sucesso.");
+            return ResponseEntity.status(201).body(response);
+        }
+        response.put("message", "Mensagem SQS não enviada.");
+        return ResponseEntity.status(503).body(response);
+    }
+
+    @PostMapping("/sns")
+    public ResponseEntity sendSns(@RequestBody MyMessage message) {
+        Boolean messageSend = messageInterface.sendSns(message);
+        Map<String, String> response = new HashMap<>();
+        if (messageSend){
+            response.put("message", "Mensagem SNS enviada com sucesso.");
+            return ResponseEntity.status(201).body(response);
+        }
+        response.put("message", "Mensagem SNS não enviada.");
+        return ResponseEntity.status(503).body(response);
     }
 }
