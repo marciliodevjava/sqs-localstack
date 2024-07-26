7fa2d5e01bcaab55b6de1a8b6eab2757bbe9d3a4
marciliodevjava <marciliodevjava@gmail.com>
2024-07-26 16:40:03 -0300

    sns

--- src/main/java/com/sqs/config/SnsConfig.java
+++ src/main/java/com/sqs/config/SnsConfig.java
@@ -3,12 +3,21 @@ package com.sqs.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.sns.SnsAsyncClient;
 import software.amazon.awssdk.services.sns.SnsClient;
 
 import java.net.URI;
 
 @Configuration
 public class SnsConfig {
+    @Bean
+    public SnsAsyncClient snsAsyncClient() {
+        return SnsAsyncClient.builder()
+                .endpointOverride(URI.create("http://localhost:4566")) // Endpoint do LocalStack
+                .region(Region.SA_EAST_1) // Região padrão para LocalStack
+                .build();
+    }
+
     @Bean
     public SnsClient snsClient() {
         return SnsClient.builder()
