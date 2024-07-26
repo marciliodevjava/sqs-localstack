9b5211409bcf6cb088ef588a7cc30889752f4832
marciliodevjava <marciliodevjava@gmail.com>
2024-07-26 16:35:40 -0300

    MessageInterface

--- src/main/java/com/sqs/service/impl/MessageInterface.java
+++ src/main/java/com/sqs/service/impl/MessageInterface.java
@@ -1,6 +1,7 @@
 package com.sqs.service.impl;
 
 import com.sqs.record.MyMessage;
+import software.amazon.awssdk.services.sns.SnsClient;
 
 public interface MessageInterface {
     Boolean sendSqs(MyMessage messageSendDto);
